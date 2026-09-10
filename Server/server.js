c53b4efdc041d0ec25bf304010109f4aa67619d5
bertilmoller <bertil@selmas.dk>
2026-09-10 10:53:40 +0200

    Lavet til Ø4 5c. Server logik er blevet implementeret med bedre svarlogik samt har jeg begydnt at lave categorier på dataen

--- Server/server.js
+++ Server/server.js
@@ -16,40 +16,72 @@ server.use(express.json());
 server.use(express.static(path.join(import.meta.dirname, '../client/public')));
 
 function findAnswer(question) {
-    const answerObj = answers.find(item => item.question.toLowerCase() === question.toLowerCase());
-    return answerObj ? answerObj.answer : "I'm sorry, I don't have an answer for that question.";     
+    const normalizedQuestion = question.toLowerCase();
+    const answerObj = answers.find(item => item.keywords.some((keyword) => normalizedQuestion.includes(keyword)));
+    return answerObj ? answerObj.answer : "I'm sorry, I don't have an answer for that question.";
 }
 
-// Endpoint to get the answers
-const questions = [];
-
-
-
-server.get("/", (req,res) => {
-    res.render("index", { 
-        question: "",
-        answer: "",
-        error : ""
+function countMatches(keywords, normalizedQuestion) {
+    const matches = keywords.filter((keyword) => {
+        if (normalizedQuestion.includes(keyword)) {
+            return true;
+        } else {
+            return false;
+        }
     });
+    return matches.length;
+}
+function findBestAnswer(question) {
+    const normalizedQuestion = question.toLowerCase();
+    let bestScore = 0;
+    let bestAns = "Intet svar";
+    let bestCategory = "";
+
+    for (const answerGroup of answers) {
+        const currScore = countMatches(answerGroup.keywords, normalizedQuestion);
+        if (currScore > bestScore) {
+            bestCategory = answerGroup.category;
+            bestScore = currScore;
+            bestAns = answerGroup.answer;
+        }
+    }
+    return {
+        answer: bestAns,
+        category: bestCategory
+    };
+}
+
+const messageList = [];
+
+const topicStats = {
+    navn: 0,
+    bosted: 0,
+    fritid: 0
+};
+
+// Endpoint to get the answers
+server.get("/", (req, res) => {
+    res.render("index", { messageList, error: "" });
 });
 
 server.post('/ask', (req, res) => {
     console.log(req.body);
-
     const question = req.body.question;
-    const answer = findAnswer(question);
-    console.log(answer);
-
-    let error ="";
-    if(!question || question.trim() ===""){
-        error = "Skriv et spørgsmål nørd";
+    let error = "";
+    if (!question) {
+        error = "skriv et spørgsmål nørd";
+    } else {
+        const result = findBestAnswer(question);
+        console.log(answer);
+        messageList.push({ type: "question", text: question });
+        messageList.push({ type: "answer", text: result.answer })
     }
+    res.render("index", { messageList, error });
+});
 
-    res.render("index", { 
-        question: question,
-        answer: answer,
-        error: error
-     });
+server.post('/clearMessages', (req, res) => {
+    messageList.length = 0;
+    res.redirect('/');
 });
 
 server.listen(port, () => {
